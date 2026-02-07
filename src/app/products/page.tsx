8019288b95a3ef8b127dfaa15e4bf922a0c6c852
jayeshh298 <officaljayesh5452@gmail.com>
2026-02-07 17:31:05 +0530

    Add products page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+import { ProductList } from "@/components/product-list";
+
+export default function ProductsPage() {
+  return (
+    <div className="container mx-auto py-6">
+      <ProductList />
+    </div>
+  );
+}
